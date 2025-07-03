cee7889865bec8a8c895b9a3ad4cb6c9f8180c44
Enrique Martínez Agudelo <enrique.martinezagudelo@externos-co.cencosud.com>
2025-07-03 18:05:46 -0500

    feat: agrega función `main` en "main.py"
    Agrega la función `main` en el módulo "main.py".
    Agrega uso de logging.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from config import start_date, end_date
+from config import start_date, end_date, log_console_level, log_file_level
+from config.logger_config import setup_logger
 from utils import create_documents, create_report, read_negative_invoices
 
 
-if __name__ == "__main__":
+logger = setup_logger(__name__, console_level=log_console_level, file_level=log_file_level)
+
+
+def main():
     documents = read_negative_invoices(start_date, end_date)
+    logger.info(f"Documents retrieved successfully: {len(documents)}")
     documents = create_documents(documents)
+    logger.info(f"Documents created successfully: {len(documents)}")
     create_report(documents)
+    logger.info("Report created successfully.")
+
+
+if __name__ == "__main__":
+    main()
